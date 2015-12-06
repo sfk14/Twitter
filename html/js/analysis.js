025b1f35fe3504d99846e2c852b2af053f83c872
sfk14 <shawnkurta@Shawns-MBP.fios-router.home>
2015-12-06 13:23:07 -0500

    JavaScript

--- html/js/analysis.js
+++ html/js/analysis.js
@@ -1,13 +1,16 @@
 function init(){
-    var schlumpyCheck = document.getElementByID("schlumpyCheck");
-    var mephistCheck = document.getElementById("mephistCheck");
-    schlumpyCheck.addEventListener('change', checkChange, false);
-}
-function checkChange(){
-    if(this.check){
-        
-    }else{
-        
+    var checks = document.getElementsByTagName("input");
+    for(var i = 0; i < checks.length; i++){
+        checks[i].addEventListener('change', selection_made, false);
     }
 }
+function selection_made(){
+    var selection = document.getElementById(this.value);
+    if(this.checked){
+        selection.style.visibility = "visible";
+    }else{
+        selection.style.visibility = "hidden";
+    }
+}
+
 window.onload = init;
