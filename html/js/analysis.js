9f10d6745cb789f30e66a4d0b5fd5adc648417ce
sfk14 <shawnkurta@Shawns-MBP.fios-router.home>
2015-12-06 13:07:12 -0500

    moved JavaScript

--- html/js/analysis.js
+++ html/js/analysis.js
@@ -1,4 +1,13 @@
 function init(){
     var schlumpyCheck = document.getElementByID("schlumpyCheck");
+    var mephistCheck = document.getElementById("mephistCheck");
+    schlumpyCheck.addEventListener('change', checkChange, false);
+}
+function checkChange(){
+    if(this.check){
+        
+    }else{
+        
+    }
 }
 window.onload = init;
