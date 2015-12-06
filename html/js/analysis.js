65d4843b107b8686f58174ddb81dd95c500a277f
sfk14 <shawnkurta@Shawns-MBP.fios-router.home>
2015-12-06 16:01:29 -0500

    JavaScript

--- html/js/analysis.js
+++ html/js/analysis.js
@@ -1,7 +1,7 @@
 function init(){
     var checks = document.getElementsByTagName("input");
     for(var i = 0; i < checks.length; i++){
-        checks[i].addEventListener('click', selection_made, false);
+        checks[i].addEventListener('change', selection_made, false);
         if(checks[i].value == "usage"){
             checks[i].checked = "true";
         }
@@ -9,8 +9,19 @@ function init(){
 }
 function selection_made(){
     var selection = document.getElementById(this.value);
+    var usageInfo = document.getElementById("usageDescription");
+    var retweetInfo = document.getElementById("retweetFavoriteDescription");
     if(this.checked){
         selection.style.visibility = "visible";
+        if(this.value == "usage"){
+            usageInfo.style.display = "block";
+            retweetInfo.style.display = "none";
+            document.getElementById("retweetFavorite");
+        }else if(this.value == "retweetFavorite"){
+            retweetInfo.style.display = "block";
+            usageInfo.style.display = "none";
+            document.getElementById("usage");
+        }
     }else{
         selection.style.visibility = "hidden";
     }
